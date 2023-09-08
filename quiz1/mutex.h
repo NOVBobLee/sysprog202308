bf5de29481b7a937bae2775b721872c39ecc33ce
NOVBobLee <defru04002@gmail.com>
2023-09-08 16:11:29 +0800

    Implement the mutex protocol configuration

--- quiz1/mutex.h
+++ quiz1/mutex.h
@@ -29,9 +29,15 @@
 #include "futex.h"
 #include "spinlock.h"
 
-typedef struct {
+typedef struct _mutex_t mutex_t;
+
+struct _mutex_t {
     atomic int state;
-} mutex_t;
+    int protocol;
+    bool (*trylock)(mutex_t *);
+    void (*lock)(mutex_t *);
+    void (*unlock)(mutex_t *);
+};
 
 typedef struct {
     int protocol;
@@ -47,17 +53,69 @@ enum {
     PRIO_INHERIT = 1 << 0,
 };
 
-#define MUTEX_INITIALIZER \
-    {                     \
-        .state = 0        \
+static inline void mutex_attr_init(mutexattr_t *attr)
+{
+    attr->protocol = PRIO_NONE;
+}
+
+static inline void mutex_attr_setprotocol(mutexattr_t *attr, int protocol)
+{
+    attr->protocol = protocol;
+}
+
+static bool _mutex_trylock(mutex_t *);
+static inline void _mutex_lock(mutex_t *);
+static inline void _mutex_unlock(mutex_t *);
+static bool _mutex_trylock_pi(mutex_t *);
+static inline void _mutex_lock_pi(mutex_t *);
+static inline void _mutex_unlock_pi(mutex_t *);
+
+#define MUTEX_INITIALIZER                                             \
+    {                                                                 \
+        .state = 0, .protocol = PRIO_NONE, .trylock = _mutex_trylock, \
+        .lock = _mutex_lock, .unlock = _mutex_unlock                  \
     }
 
 static inline void mutex_init(mutex_t *mutex, mutexattr_t *attr)
 {
     atomic_init(&mutex->state, 0);
+
+    if (attr)
+        mutex->protocol = attr->protocol;
+    else
+        mutex->protocol = PRIO_NONE;
+
+    switch (mutex->protocol) {
+    case PRIO_INHERIT:
+        mutex->trylock = _mutex_trylock_pi;
+        mutex->lock = _mutex_lock_pi;
+        mutex->unlock = _mutex_unlock_pi;
+        break;
+    case PRIO_NONE:
+    default:
+        mutex->trylock = _mutex_trylock;
+        mutex->lock = _mutex_lock;
+        mutex->unlock = _mutex_unlock;
+        break;
+    }
 }
 
 static bool mutex_trylock(mutex_t *mutex)
+{
+    return mutex->trylock(mutex);
+}
+
+static inline void mutex_lock(mutex_t *mutex)
+{
+    mutex->lock(mutex);
+}
+
+static inline void mutex_unlock(mutex_t *mutex)
+{
+    mutex->unlock(mutex);
+}
+
+static bool _mutex_trylock(mutex_t *mutex)
 {
     int state = load(&mutex->state, relaxed);
     if (state & MUTEX_LOCKED)
@@ -71,11 +129,11 @@ static bool mutex_trylock(mutex_t *mutex)
     return true;
 }
 
-static inline void mutex_lock(mutex_t *mutex)
+static inline void _mutex_lock(mutex_t *mutex)
 {
 #define MUTEX_SPINS 128
     for (int i = 0; i < MUTEX_SPINS; ++i) {
-        if (mutex_trylock(mutex))
+        if (_mutex_trylock(mutex))
             return;
         spin_hint();
     }
@@ -90,24 +148,22 @@ static inline void mutex_lock(mutex_t *mutex)
     thread_fence(&mutex->state, acquire);
 }
 
-static inline void mutex_unlock(mutex_t *mutex)
+static inline void _mutex_unlock(mutex_t *mutex)
 {
     int state = exchange(&mutex->state, 0, release);
     if (state & MUTEX_SLEEPING)
         futex_wake(&mutex->state, 1);
 }
 
-static bool mutex_trylock_pi(mutex_t *mutex)
+static bool _mutex_trylock_pi(mutex_t *mutex)
 {
     return true;
 }
-static inline void mutex_lock_pi(mutex_t *mutex) {}
-static inline void mutex_unlock_pi(mutex_t *mutex) {}
+static inline void _mutex_lock_pi(mutex_t *mutex) {}
+static inline void _mutex_unlock_pi(mutex_t *mutex) {}
 
 /* dummy */
 static inline void mutex_destroy(mutex_t *mutex) {}
-static inline void mutex_attr_init(mutexattr_t *attr) {}
 static inline void mutex_attr_destroy(mutexattr_t *attr) {}
-static inline void mutex_attr_setprotocol(mutexattr_t *attr, int protocol) {}
 
 #endif
